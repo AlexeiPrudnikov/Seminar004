4406e8f3c2b87831634c371b33d3e730d2d0adb2
Alexei <alexei.prudnikov@gmail.com>
2022-03-21 23:16:54 +0300

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿int GetSumDigits(int number)
+{
+    int sum = 0;
+    number = Math.Abs(number);
+    do
+    {
+        sum = sum + (number % 10);
+        number /= 10;
+    }
+    while (number > 0);
+    return sum;
+}
+Console.WriteLine("==========Задача № 27==========");
+Console.WriteLine("Напишите программу, которая принимает на вход число, а потом возвращает сумму цифр в числе");
+int number = 0;
+try
+{
+    Console.Write("Введите число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ($"Сумма цифр числа {number} равна {GetSumDigits(number)}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
